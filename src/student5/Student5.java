a46c6139bda60bfb40da3f613adb4e46060c288d
William Long <read.longwil@sheridancollege.ca>
2021-02-02 10:53:07 -0500

    First commit

---
+++ src/student5/Student5.java
@@ -0,0 +1,16 @@
+
+package student5;
+
+
+public class Student5 {
+
+
+    public static void main(String[] args) {
+        String[] students= {"Tim", "David", "Joe", "Anthony", "James"};
+        
+        for (int i =students.length -1; i >=0; i--){
+            System.out.println(students[i]);
+        }
+    }
+    
+}
